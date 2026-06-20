c172fc7972f52e761c5f725b42456ce7230db93a
yuliethdmguez05 <yuliethdominguez102@gmail.com>
2026-06-20 17:42:29 -0500

    Subiendo cambios del proyecto EC en la rama subirevidencias

---
+++ Models/Logro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EC.Models
+{
+    public class Logro
+    {
+        [Key]
+        public int IdLogro { get; set; }
+        public string NombreLogro { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public string Icono { get; set; } = string.Empty;
+        public DateTime FechaObtencion { get; set; }
+
+        // ✅ Relación con Estudiantes
+        public int IdEstudiante { get; set; }
+        public Estudiantes Estudiante { get; set; }
+    }
+}
